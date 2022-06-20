88a83b4f9451006ee6f3b96ce84d76e1e6d95763
Dean Cobb <deaco@DEACOs-MacBook-Pro.local>
2022-06-20 12:07:17 +0100

    version created + ready for design

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Get help to buy food and milk (Healthy Start)',
 
   // Port to run nodemon on locally
   port: 2000,
