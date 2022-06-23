0074c5fb94302e691418820486849720a6096eab
SteLV426 <stephen.mccartney@nhs.net>
2022-06-23 15:00:44 +0100

    Personal details screens

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,17 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/v1/your-name', function (req, res) {
+
+    const firstName = req.session.data['first-name']
+    const lastName = req.session.data['last-name']
+
+    if (firstName && lastName) {
+       res.redirect('/v1/personal-details/cya-personal-details');
+    } else {
+        res.redirect('/v1/personal-details/personsdetails');
+    }
+
+})
+
 module.exports = router;
