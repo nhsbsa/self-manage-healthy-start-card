09124a2d702d2de04e5075775f13e2da35fac4b5
Dean Cobb <deaco@DEACOs-MacBook-Pro.local>
2022-06-23 15:07:51 +0100

    Healthy start card managing screens

--- app/views/v1/_routes.js
+++ app/views/v1/_routes.js
@@ -1,9 +1,21 @@
+// ********************************
+// APPLY (v1)
+// ********************************
+
 // External dependencies
 const express = require('express');
-
 const router = express.Router();
+const moment = require('moment');
+const stringSimilarity = require("string-similarity");
+const geolib = require('geolib');
+const https = require("https");
 
-// Add your routes here - above the module.exports line
+// CONSTANTS
+const today = new Date(Date.now());
+const { listenerCount } = require('gulp');
+
+// ********************************
+
 
 router.post('/v1/child-name', function (req, res) {
 
@@ -17,5 +29,3 @@ router.post('/v1/child-name', function (req, res) {
     }
   
 })
-
-module.exports = router;
