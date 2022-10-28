acca68fdcdfaa736453a525bcf460339fb0ca1e1
SteLV426 <stephen.mccartney@nhs.net>
2022-10-28 10:10:20 +0100

    v3

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 // Route File Versions
 // ****************************************
 
+router.use('/v3', require('./views/v3/_routes'));
 router.use('/v2', require('./views/v2/_routes'));
 router.use('/v1', require('./views/v1/_routes'));
 
